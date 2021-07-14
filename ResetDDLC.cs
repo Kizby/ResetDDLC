67650dc43881f03d955360a8ab18b015023e988b
Αθηνα 𒂗𒃶𒌌𒀭𒈾 Washington <18bitsofentropy+gitkraken@gmail.com>
2021-07-14 14:01:02 -0400

    Make ResetDDLC mod that maybe resets the game

---
+++ ResetDDLC.cs
@@ -0,0 +1,28 @@
+﻿
+using BepInEx;
+using HarmonyLib;
+using RenpyLauncher;
+
+namespace TestDDLCMod
+{
+    [BepInPlugin("org.kizbyspark.plugins.resetDDLC", "Reset DDLC", "0.1.0.0")]
+    [BepInProcess("Doki Doki Literature Club Plus.exe")]
+    public class ResetDDLC : BaseUnityPlugin
+    {
+        void Awake()
+        {
+            Harmony harmony = new Harmony("org.kizbyspark.plugins.resetDDLC");
+            harmony.PatchAll();
+        }
+    }
+
+
+    public static class DoReset
+    {
+        [HarmonyPatch(typeof(LauncherMain), "Start")]
+        static void Postfix()
+        {
+            LauncherParameters.ResetDDLC = true;
+        }
+    }
+}
